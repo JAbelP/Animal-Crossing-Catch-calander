76584cc87c8defe8f032c5b0ce6272591b1e8cda
Abel P <jpinales@uncc.edu>
2020-03-30 02:53:20 -0400

    Updated how the final print out, took out some debugging code added Flix's prices, added after hour pricesUpdated how the final print out, took out some debugging code added Flix's prices, added after hour pricesz

--- AnimalCrossing.py
+++ AnimalCrossing.py
@@ -8,10 +8,31 @@ import datetime
     Price of item (Nooks shop, after hours, and special buyer)
     List them based on price. 
     List them based on Name.
-    Time function - Current Time and what ever time the user wants.
-    Check if the month is correct (Maybe change from Name date to Numerical Date?)
+    Add Fishes
+
     Time issues:
     Emperor ButterFly
+    Atlas Moth
+    cricket
+    evening cicada
+    Firefly
+    Giant Water bug
+    Scarab beetle
+    goliath beetle
+    Giant stag
+    Rainbow stag
+    cyclommatus stag
+    golden stag
+    giraffe stag
+    Horned dynastid
+    horned atlas
+    horned elephant
+    horned hercules
+    walking stick
+    mosquito
+    Pill bug
+    Spider
+    Scorpion
     Moth
     Man-Faced Stinkbug
     Tarantula
@@ -24,6 +45,7 @@ Fishes = []
 BugsA=[]
 FishesA=[]
 timeHour = None
+timeMonth= None
 
 
 class Bug:
@@ -34,13 +56,16 @@ class Bug:
         self.endTime   = endTime
         self.Months    = Months
         self.price     = price
-    
+        self.flickPrice= int(price*1.50)
+        self.afterHourPrice = int(price*.80)
+
+
     def printNoDetail(self):
-        return "Name: {}\nPrice: {}\nCan catch until: {}".format(self.name,self.price,self.endTime)
+        return "Name: {}\nPrice: {}\nCan catch until: {}\nflick's price: {}\nAfter hour price: {}".format(self.name,self.price,self.endTime,self.flickPrice,self.afterHourPrice)
 
     def __str__(self):
        
-     return "Type: {} \nName: {} \nYou can start catching at: {}\nYou will no longer catch at: {}\nSeasons avaliable: {}".format(self.type,self.name,self.startTime,self.endTime,self.Months)
+     return "Type: {} \nName: {} \nSeasons avaliable: {}".format(self.type,self.name,self.Months)
 
 class Fish:
     def __init__(self,Ctype,name,startTime,endTime,month,Location):
@@ -60,24 +85,28 @@ def whatIsAvaliableBTime():
     '''
     print("Use the current time or have you time skipped?")
     TimeValue = 0
+    
     while TimeValue>2 or TimeValue<1:
         TimeValue = int(input("1 for Computer time/ 2 for own time: "))
     if (TimeValue == 1):
         timeHour= datetime.datetime.now().hour
-        print("Abel")
+        timeMonth = datetime.datetime.now().month
+        
 
     if (TimeValue == 2):
         timeHour = int(input("Enter in the hour in the 24 hour format.(ie.10 pm is now 22: "))
+        timeMonth = int(input("What month is it (give me the numerical value of month (October = 10)): "))
+
 
     #make a list
     for insect in Bugs:
         if (insect.startTime<timeHour and insect.endTime>timeHour):
-            if(datetime.datetime.now().month in insect.Months):
+            if(timeMonth in insect.Months):
                 BugsA.append(insect)
 
     for creature in Fishes:
         if (creature.startTime<timeHour and creature.endTime>timeHour):
-            if(datetime.datetime.now().month in creature.Months):
+            if(timeMonth in creature.month):
                 FishesA.append(creature)
     
 def creatingList():
@@ -91,7 +120,7 @@ def creatingList():
         #adding bugs to the list
         for b in data['type']['bug']:
             
-            Bugs.append(Bug("Bug",b['name'],b['Start-time'],b['End-Time'],b['Season']))
+            Bugs.append(Bug("Bug",b['name'],b['Start-time'],b['End-Time'],b['Season'],b['price']))
 
         #adding fishes
         for f in data['type']['fish']:
